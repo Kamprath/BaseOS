fa9c3083150ab779f53ba38e49fdf05ca3da0c72
ravroid <johnnykamprath@gmail.com>
2014-12-28 22:59:58 -0600

    Add TControl default program to repository

---
+++ BaseOS/programs/tcontrol.lua
@@ -0,0 +1,158 @@
+--[[
+-- TurtleControl provides an interface for remotely controlling a turtle
+--
+-- Requirements:
+--   * Turtle component
+--   * BaseOS v1.1 or newer
+--]]
+
+-- register program with BaseOS
+BaseOS.programs['TControl'] = function(self, ...)
+    -- define the program
+    local TurtleControl = {
+        data = {
+            window = {width = nil, height = nil},       -- window information
+            keys = {                                    -- key codes
+                up = 200,
+                down = 208,
+                enter = 28,
+                space = 57,
+                ctrl = 29,
+                alt = 56,
+                w = 17,
+                a = 30,
+                s = 31,
+                d = 32
+            },
+            turtleID = nil,                             -- ID of the currently-connected turtle
+            selection = 1,                              -- Current menu selection,
+            activeMenuKey = nil                         -- The array key of an object in the 'menus' table
+        },
+        menus = {
+            ['main'] = {
+                ['Enter direct control mode'] = function(self)
+                    term.clear();
+                    term.setCursorPos(1, 1);
+                    print('Use W, A, S, D, Space, and Ctrl to move around.\n\nPress Enter to dig.\n\nPress Alt to exit direct control mode.');
+
+                    while (true) do
+                        local e, key = os.pullEvent('key');
+
+                        if (key == self.data.keys.w) then
+                            self.menus['move']['forward'](self);
+                        elseif (key == self.data.keys.s) then
+                            self.menus['move']['back'](self);
+                        elseif (key == self.data.keys.a) then
+                            self.menus['move']['left'](self);
+                        elseif (key == self.data.keys.d) then
+                            self.menus['move']['right'](self);
+                        elseif (key == self.data.keys.ctrl) then
+                            self.menus['move']['down'](self);
+                        elseif (key == self.data.keys.space) then
+                            self.menus['move']['up'](self);
+                        elseif (key == self.data.keys.enter) then
+                            self.menus['move']['dig'](self);
+                        elseif (key == self.data.keys.alt) then
+                            break;
+                        end
+                    end
+                end,
+                ['Dump inventory'] = function(self)
+
+                end,
+                ['Get fuel level'] = function(self)
+
+                end
+            }
+        },
+
+        init = function(self)
+            self.data.window.width, self.data.window.height = term.getSize();
+
+            -- establish a connection to the turtle
+            BaseOS:cwrite('Specify turtle ID to connect to: ', colors.white);
+            self.data.turtleID = tonumber(io.read());
+            -- todo: send 'connect' message to turtle
+
+            -- set the menu that should be displayed. This should change based on what you want this program to do
+            self.data.activeMenuKey = 'main';
+
+            -- run the event-listening loop
+            self:startLoop();
+        end,
+
+        --- Main loop to run the program
+        startLoop = function(self)
+            while (true) do
+                -- draw the interface
+                self:drawInterface();
+
+                -- wait for event
+                self:handleEvent(os.pullEvent('key'));
+            end
+        end,
+
+        --- Calls methods that correspond to certain event types
+        handleEvent = function(self, ...)
+            -- remove first argument from arguments table
+            local eventType = table.remove(arg, 1);
+
+            if (eventType == 'key') then
+                local key = arg[1];
+                local selection = self.data.selection;
+                local menuSize = BaseOS:tableSize(self.menus[self.data.activeMenuKey]);
+
+                if ((key == self.data.keys.up or key == self.data.keys.w) and (selection - 1) >= 1) then
+                    self.data.selection = selection - 1;
+                elseif ((key == self.data.keys.down or key == self.data.keys.d) and (selection + 1) <= menuSize) then
+                    self.data.selection = selection + 1;
+                elseif (key == self.data.keys.enter) then
+                    self:doMenuAction(self.menus[self.data.activeMenuKey], self.data.selection);
+                end
+            end
+        end,
+
+        doMenuAction = function(self, menu, optionNum)
+            local i = 1;
+            for key, val in pairs(menu) do
+                if (optionNum == i) then
+                    val(self);
+                    return true;
+                end
+
+                i = i + 1;
+            end
+            return false;
+        end,
+
+        -- draws a menu to the terminal
+        drawMenu = function(self, menu, selection)
+            local menuSize = BaseOS:tableSize(menu);
+            term.setCursorPos(1, math.floor((self.data.window.height - (menuSize * 2)) / 2));
+
+            local i = 1;
+            for key, val in pairs(menu) do
+                -- prefix active menu choice
+                if (i == selection) then
+                    BaseOS:cwrite(' >  ', colors.white);
+                else
+                    write('    ');
+                end
+
+                i = i + 1;
+                write(key .. '\n\n');
+            end
+        end,
+
+        --- Calls methods that draw things to the terminal
+        drawInterface = function(self)
+            term.clear();
+
+            -- draw self.data.activeMenu as menu
+            self:drawMenu(self.menus[self.data.activeMenuKey], self.data.selection);
+        end
+    }
+
+    -- start the program
+    TurtleControl:init();
+end
